0d4f8dda0e1e5d7b16582efaa7b2e496fee26511
Anthony Rodriguez <anthony@nezia.dev>
2023-11-30 12:32:52 +0100

    update index to use new function return

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import * as OTPAuth from 'otpauth'
+import { encrypt, decrypt } from './utils'
+
+// OTP
+const totp = OTPAuth.URI.parse(
+  'otpauth://totp/Missive:OTPAuth?issuer=Missive&secret=QFEPVWPD4OXBPKTZCOUPIZ4URDI223VN&algorithm=SHA256&digits=6&period=30'
+)
+console.log(totp.validate({ token: '519333' }))
+
+console.log('Hello via Bun!')
+
+const encryptedTOTP = await encrypt(totp.toString(), 'sample-password')
+decrypt(encryptedTOTP.text, 'sample-password')
