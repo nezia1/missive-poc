07ab9555050748b9a6635b1658ce835a970e6589
Anthony Rodriguez <anthony@nezia.dev>
2023-12-06 13:24:18 +0100

    register users route

--- server/index.ts
+++ server/index.ts
@@ -3,13 +3,18 @@ import * as OTPAuth from 'otpauth'
 import { PrismaClient } from '@prisma/client'
 import { password } from 'bun'
 import { encrypt, decrypt } from '@/utils'
+import { parseGenericError } from '@/utils'
+
+import users from '@/routes/users'
 
 const prisma = new PrismaClient()
 
 const fastify = Fastify({ logger: true })
 
+fastify.register(users, { prefix: '/users' })
+
 try {
-  await fastify.listen({ port: 3000 })
+  await fastify.listen({ port: 8080 })
 } catch (err) {
   fastify.log.error(err)
   process.exit(1)
