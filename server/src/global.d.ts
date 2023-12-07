2c07d58e94577e546d4361957d132f8ab517b5f5
Anthony Rodriguez <anthony@nezia.dev>
2023-12-07 22:50:09 +0100

    simplify type (dont assume user gave both)

--- server/src/global.d.ts
+++ server/src/global.d.ts
@@ -3,15 +3,7 @@ import { User } from '@prisma/client'
 export interface ResourceParams {
   id: string
 }
-interface BasicUpdateUserBody extends User {
-  enable_totp?: false
+export interface UpdateUserBody {
+  enable_totp?: boolean
   password?: string
 }
-
-interface UpdateUserBodyWithTOTP extends User {
-  enable_totp: true
-  password: string
-}
-
-// This union type exists so that if a user has enable_totp set to true, the password field is required
-export type UpdateUserBody = BasicUpdateUserBody | UpdateUserBodyWithTOTP
