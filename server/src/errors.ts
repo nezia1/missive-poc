97eb22c286930bb8d9c4b6a20816e610d6486aac
Anthony Rodriguez <anthony@nezia.dev>
2023-12-07 08:36:15 +0100

    simplify default argument

--- server/src/errors.ts
+++ server/src/errors.ts
@@ -1,6 +1,6 @@
 export class AuthenticationError extends Error {
   id?: string
-  constructor(message: string, { id }: { id?: string } = { id: undefined }) {
+  constructor(message: string, { id }: { id?: string } = {}) {
     super(message)
     this.name = 'AuthenticationError'
     this.id = id
