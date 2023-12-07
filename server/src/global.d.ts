bf96a75310979956380f9e33bf1173ad5170af3e
Anthony Rodriguez <anthony@nezia.dev>
2023-12-07 22:34:14 +0100

    add union type for update body

--- server/src/global.d.ts
+++ server/src/global.d.ts
@@ -3,7 +3,14 @@ import { User } from '@prisma/client'
 export interface ResourceParams {
   id: string
 }
-
-export interface UpdateUserBody extends User {
-  enable_totp: boolean
+interface BasicUpdateUserBody extends User {
+  enable_totp?: false
 }
+
+interface UpdateUserBodyWithTOTP extends User {
+  enable_totp: true
+  totp_password: string
+}
+
+// This union type exists so that if a user has enable_totp set to true, the password field is required in order to be able to encrypt the TOTP secret using the user's password
+export type UpdateUserBody = BasicUpdateUserBody | UpdateUserBodyWithTOTP
