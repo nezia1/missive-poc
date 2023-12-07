3823ee1872597424035cd09015b7a7959afc71cd
Anthony Rodriguez <anthony@nezia.dev>
2023-12-07 08:35:12 +0100

    add cookies and check for JWT_COOKIE env

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,9 @@
 import Fastify from 'fastify'
 import * as OTPAuth from 'otpauth'
 import { PrismaClient } from '@prisma/client'
+import cookie from '@fastify/cookie'
+import type { FastifyCookieOptions } from '@fastify/cookie'
+
 import { password } from 'bun'
 import { encrypt, decrypt } from '@/utils'
 import { parseGenericError } from '@/utils'
@@ -12,8 +15,17 @@ const prisma = new PrismaClient()
 
 const fastify = Fastify({ logger: true })
 
+if (process.env.COOKIE_SECRET === undefined) {
+  console.error('COOKIE_SECRET is not defined')
+  process.exit(1)
+}
+
 fastify.register(users, { prefix: '/users' })
 fastify.register(tokens, { prefix: '/tokens' })
+fastify.register(cookie, {
+  secret: process.env.COOKIE_SECRET,
+  parseOptions: {},
+} as FastifyCookieOptions)
 
 try {
   await fastify.listen({ port: 8080 })
