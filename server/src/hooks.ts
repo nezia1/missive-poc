76a60e99259d30f8520c92bc82f3d87fee3fafd7
Anthony Rodriguez <anthony@nezia.dev>
2023-12-07 10:21:44 +0100

    add authentication hook

---
+++ server/src/hooks.ts
@@ -0,0 +1,46 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { jwtVerify } from 'jose'
+import { JWSSignatureVerificationFailed, JWTExpired } from 'jose/errors'
+import { PrismaClient } from '@prisma/client'
+
+import { AuthenticationError } from '@/errors'
+const prisma = new PrismaClient()
+
+if (process.env.JWT_SECRET === null) {
+  console.error('JWT_SECRET is not defined')
+  process.exit(1)
+}
+
+const secret = new TextEncoder().encode(process.env.JWT_SECRET)
+
+export async function authenticationHook(
+  request: FastifyRequest,
+  reply: FastifyReply
+): Promise<void> {
+  // Check for refresh token
+  const refreshTokenCookie = request.cookies.refreshToken
+
+  if (!refreshTokenCookie)
+    throw new AuthenticationError('Missing refresh token')
+
+  // Get access token from Authorization header
+  let accessToken = request.headers.authorization?.split(' ')[1]
+
+  if (!accessToken) throw new AuthenticationError('Missing access token')
+
+  try {
+    // Get access token payload and check if it matches a user (jwtVerify throws an error if the token is invalid for any reason)
+    const { payload } = await jwtVerify(accessToken, secret)
+    const user = await prisma.user.findUnique({ where: { id: payload.sub } })
+
+    if (user === null) throw new AuthenticationError('Invalid access token')
+  } catch (error) {
+    if (error instanceof JWTExpired) {
+      throw new AuthenticationError('Access token expired')
+    } else if (error instanceof JWSSignatureVerificationFailed) {
+      throw new AuthenticationError('The access token has been tampered with')
+    } else {
+      throw new AuthenticationError('Invalid access token')
+    }
+  }
+}
