ebec84f0b957976a6643ef0862473d467aae0267
Anthony Rodriguez <anthony@nezia.dev>
2023-12-11 13:19:17 +0100

    let fastify handle error handling

--- server/src/hooks.ts
+++ server/src/hooks.ts
@@ -1,6 +1,10 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { jwtVerify } from 'jose'
-import { JWSSignatureVerificationFailed, JWTExpired } from 'jose/errors'
+import {
+  JWSSignatureVerificationFailed,
+  JWTExpired,
+  JWTInvalid,
+} from 'jose/errors'
 import { PrismaClient } from '@prisma/client'
 
 import { AuthenticationError } from '@/errors'
@@ -29,35 +33,19 @@ export async function authenticationHook(
   request: FastifyRequest,
   reply: FastifyReply
 ): Promise<void> {
-  // Check for refresh token
-  const refreshTokenCookie = request.cookies.refreshToken
-
-  if (!refreshTokenCookie)
-    throw new AuthenticationError('Missing refresh token')
-
   // Get access token from Authorization header
   let accessToken = request.headers.authorization?.split(' ')[1]
 
-  if (!accessToken) throw new AuthenticationError('Missing access token')
+  if (!accessToken) throw new JWTInvalid('Missing access token')
 
-  try {
-    // Get access token payload and check if it matches a user (jwtVerify throws an error if the token is invalid for any reason)
-    const { payload } = await jwtVerify(accessToken, secret)
-    const user = await prisma.user.findUniqueOrThrow({
-      where: { id: payload.sub },
-    })
+  // Get access token payload and check if it matches a user (jwtVerify throws an error if the token is invalid for any reason)
+  const { payload } = await jwtVerify(accessToken, secret)
+  const user = await prisma.user.findUniqueOrThrow({
+    where: { id: payload.sub },
+  })
 
-    // Inject the authenticated user ID in the request
-    request.authenticatedUser = {
-      id: user.id,
-    }
-  } catch (error) {
-    if (error instanceof JWTExpired) {
-      throw new AuthenticationError('Access token expired')
-    } else if (error instanceof JWSSignatureVerificationFailed) {
-      throw new AuthenticationError('The access token has been tampered with')
-    } else {
-      throw new AuthenticationError('Invalid access token')
-    }
+  // Inject the authenticated user ID in the request
+  request.authenticatedUser = {
+    id: user.id,
   }
 }
