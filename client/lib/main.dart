be3c0911dff8356cf0b50685947c5a238bf47f20
Anthony Rodriguez <anthony@nezia.dev>
2024-02-09 16:17:18 +0100

    add go_router
    For the architecture I want to build, it is crucial to use a routing package to be able to import everything in main.dart and have the full app structure visible from the entry point. I think that would make it easier to undertand and also easier to debug. go_router also comes with a lot of great features, such as being able to change the transition animation, create a catch-all error page and much more.

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'features/authentication/login_screen.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const FlutterPOC());
 }
+
+final GoRouter _router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) {
+        return const LoginScreen(title: FlutterPOC.title);
+      },
+    ),
+  ],
+);
 
 class FlutterPOC extends StatelessWidget {
   static const title = 'Flutter Auth';
@@ -12,14 +25,12 @@ class FlutterPOC extends StatelessWidget {
   // This widget is the root of your application
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
         title: title,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
           useMaterial3: true,
         ),
-        home: const LoginScreen(
-          title: title,
-        ));
+        routerConfig: _router);
   }
 }
