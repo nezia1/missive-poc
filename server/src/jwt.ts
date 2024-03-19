2eed7fd8c3f3d34adf8d6e9117e44626968a37ac
Anthony Rodriguez <anthony@nezia.dev>
2024-03-19 19:06:43 +0100

    define extended property type explicitly

--- server/src/jwt.ts
+++ server/src/jwt.ts
@@ -1,14 +1,17 @@
-import { JWTPayload, SignJWT, jwtVerify } from 'jose'
-import { Permissions } from './permissions'
+import { type JWTPayload, SignJWT, jwtVerify } from 'jose'
+import type { Permissions } from './permissions'
 
 interface ScopedJWTPayload extends JWTPayload {
-  scope?: Permissions[]
+	scope?: Permissions[]
 }
 
 export class SignScopedJWT extends SignJWT {
-  constructor(payload: ScopedJWTPayload | undefined) {
-    super(payload)
-  }
+	protected _payload: ScopedJWTPayload
+
+	constructor(payload: ScopedJWTPayload) {
+		super(payload)
+		this._payload = payload
+	}
 }
 
 /// This function is used to verify and decode a JWT token
@@ -16,9 +19,9 @@ export class SignScopedJWT extends SignJWT {
 /// @param {Uint8Array} secret - The secret to use to verify the token
 /// @returns {Promise<ScopedJWTPayload>} - The decoded payload with the scope/permissions
 export async function verifyAndDecodeJWT(
-  token: string,
-  secret: Uint8Array
+	token: string,
+	secret: Uint8Array,
 ): Promise<ScopedJWTPayload> {
-  const { payload } = await jwtVerify(token, secret)
-  return payload as ScopedJWTPayload
+	const { payload } = await jwtVerify(token, secret)
+	return payload as ScopedJWTPayload
 }
