d186560bae364f1a9b1023d497805aca907e1b30
Anthony Rodriguez <anthony@nezia.dev>
2024-03-25 11:25:24 +0100

    check if superproject exists first

--- is_submodule.sh
+++ is_submodule.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+superproject=$(git rev-parse --show-superproject-working-tree)
+
 # This script checks if the current directory is a submodule of missive (used in npm prepare to symlink biome since the VSCode extension doesn't check for subdirectories)
-[ "$(basename $(git rev-parse --show-superproject-working-tree))" = "missive" ]
+[ -n "$superproject" ] && [ "$(basename "$superproject")" = "missive" ]
