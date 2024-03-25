050bc207e3d858738bbeb5314c9d6b843cfc2b9f
Anthony Rodriguez <anthony@nezia.dev>
2024-03-25 11:48:42 +0100

    use prepare script instead of logic in prepare

---
+++ prepare.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+function is_submodule() {
+    superproject=$(git rev-parse --show-superproject-working-tree)
+
+    # This script checks if the current directory is a submodule of missive (used in npm prepare to symlink biome since the VSCode extension doesn't check for subdirectories)
+    [ -n "$superproject" ] && [ "$(basename "$superproject")" = "missive" ]
+}
+npx lefthook install && echo 'Lefthook installed.'
+
+if is_submodule; then
+    echo 'Submodule check passed.'
+    ln -f biome.json ../biome.json
+    echo 'Symlink created.'
+else
+    echo 'Submodule check failed. Symlink not created.'
+fi
