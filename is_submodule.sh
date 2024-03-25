0a398b8cc16fe62b85f67dc85e4f005904e44f6f
Anthony Rodriguez <anthony@nezia.dev>
2024-03-25 12:50:10 +0100

    remove unnecessary script

--- is_submodule.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-superproject=$(git rev-parse --show-superproject-working-tree)
-
-# This script checks if the current directory is a submodule of missive (used in npm prepare to symlink biome since the VSCode extension doesn't check for subdirectories)
-[ -n "$superproject" ] && [ "$(basename "$superproject")" = "missive" ]
